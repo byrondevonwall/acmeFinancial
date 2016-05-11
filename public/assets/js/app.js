029be622eb55fb7d9cd321ec6936e357a346e221
Byron Wall <byrondevonwall@gmail.com>
2016-05-11 18:05:29 -0400

    more routing...

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -81,6 +81,9 @@ acmeApp.config(function($routeProvider, $locationProvider, $httpProvider){
       templateUrl: '/views/profile.html',
       controller: 'profileController',
       // publicAccess: true,
+      resolve: {
+        loggedin: loginCheck
+      }
     })
     .when('/edit', {
       templateUrl: '/views/edit.html',
